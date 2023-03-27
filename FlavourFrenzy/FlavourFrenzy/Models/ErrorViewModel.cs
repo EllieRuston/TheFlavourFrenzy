84060d44144a30c027fe9bae0381c5672a76e097
EllieRuston <88114202+EllieRuston@users.noreply.github.com>
2023-03-27 17:40:18 +1300

    Delete FlavourFrenzy directory

--- FlavourFrenzy/FlavourFrenzy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlavourFrenzy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
